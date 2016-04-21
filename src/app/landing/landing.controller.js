b259630af2e9b1ed5fe3788c2cc07c7f4fe3b33b
alanbares <neloice@gmail.com>
2016-04-21 16:54:20 -0700

    Add upVote function and add votes to firebase.

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -12,26 +12,34 @@
     var firePosts = new Firebase('https://reddit-clone-alan.firebaseio.com/posts');
 
 
-
+    //View model methods
     vm.newPost = new Post();
     vm.posts = $firebaseArray(firePosts);
     vm.addPost = addPost;
     vm.deletePost = deletePost;
+    vm.upVote = upVote;
 
 
     function Post() {
-      this.name = '',
-      this.description = '',
+      this.name = '';
+      this.description = '';
       this.url = '';
+      this.voteCount = 0;
     }
 
     function addPost() {
       vm.posts.$add(vm.newPost);
       vm.newPost = new Post();
+
     }
 
     function deletePost(post) {
       vm.posts.$remove(post);
+    }
+
+    function upVote(post) {
+      post.voteCount++;
+      vm.posts.$save(post);
     }
   }
 
