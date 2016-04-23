76137aaf1e4920c4f121f14c19886ba3003e662a
alanbares <neloice@gmail.com>
2016-04-22 22:29:17 -0700

    Register new users through firebase with register function and user object then connect to register.html.

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,7 +5,23 @@
     .module('app.auth')
     .controller('AuthController', AuthController);
 
-  function AuthController () {
+  AuthController.$inject = ['$firebaseAuth'];
+
+  function AuthController ($firebaseAuth) {
+    var vm = this;
+    var firebaseReference = new Firebase('https://reddit-clone-alan.firebaseio.com/');
+    var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+    // View model methods
+    vm.register = register;
+    vm.user = {
+      email: '',
+      password: ''
+    }
+
+    function register(user) {
+      return firebaseAuthObject.$createUser(user);
+    }
 
   }
 })();
